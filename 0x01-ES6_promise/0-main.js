da0da1265259698b653260d12325f288aa57c0ee
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-07-27 00:15:11 -0700

    Task 0 update

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,4 @@
-const getResponseFromAPI = require("./0-promise.js");
+import getResponseFromAPI from "./0-promise.js";
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
-// Function that returns a Promise
-function getResponseFromAPI() {
-  // The Promise constructor takes a callback function with two parameters: resolve and reject.
+export default function getResponseFromAPI() {
+  const success = true;
   return new Promise((resolve, reject) => {
-    // Perform asynchronous operations here (e.g., API call, file reading, etc.)
-    // For simplicity, let's resolve the Promise immediately with a value.
-    resolve("API response data"); // Replace this with the actual API response data when implementing real asynchronous operations.
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
-
-module.exports = getResponseFromAPI;
